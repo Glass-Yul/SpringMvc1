69f452b98012090629a87abfe400152eeba32b5d
Glass-Yul <dbfl3742@naver.com>
2023-08-06 15:39:18 +0900

    httpRequestServlet 사용법

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 컴포넌트들을 찾아 자동으로 등록해줌
 @SpringBootApplication
 public class ServletApplication {
 
